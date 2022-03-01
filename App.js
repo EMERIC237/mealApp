1e470e9cea47c83dd2733c9b83001781d890b9d4
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-03-01 16:56:56 -0600

    stack navigation implemented and completed

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import MealsNavigator from "./navigation/MealsNavigator";
-import { usesc } from "react-native-screens";
+import { enableScreens } from "react-native-screens";
 
 enableScreens();
 const fetchFonts = () => {
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,7 @@
 import React from "react";
 import {
   StyleSheet,
-  Text,
   FlatList,
-  View,
-  TouchableOpacity,
 } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
